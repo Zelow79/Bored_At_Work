34c25143da84eab574e8e309bee9f081c17f675a
Zelow79 <zelow79@gmail.com>
2023-12-18 22:40:26 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,25 +11,34 @@ const rowCount = 2,
 function updateValues() {
     if (mObj.x < 1) mObj.x++;
     mObj.x1 += mObj.x;
-    mObj.xx = mObj.x + mObj.x;
+    mObj.xx += mObj.x;
     mObj.x2 += mObj.x1;
     mObj.xx1 += mObj.xx;
     mObj.xx2 += mObj.xx1;
 
-    document.getElementById("x").innerHTML = mObj.x;
-    document.getElementById("xx").innerHTML = mObj.xx;
-    document.getElementById("x1").innerHTML = mObj.x1;
-    document.getElementById("x2").innerHTML = mObj.x2;
-    document.getElementById("xx1").innerHTML = mObj.xx1;
-    document.getElementById("xx2").innerHTML = mObj.xx2;
-
-    //Object.entries(mObj).forEach(([key, val]) => console.log(`${key}:${val}`));
+    document.getElementById("x").innerHTML = format(mObj.x);
+    document.getElementById("xx").innerHTML = format(mObj.xx);
+    document.getElementById("x1").innerHTML = format(mObj.x1);
+    document.getElementById("x2").innerHTML = format(mObj.x2);
+    document.getElementById("xx1").innerHTML = format(mObj.xx1);
+    document.getElementById("xx2").innerHTML = format(mObj.xx2);
 }
 
 function main() {
-    setTimeout(() =>{
+    setTimeout(() => {
         updateValues();
-        //alert('success');
         main();
     }, 1000);
+}
+
+function format(value, maxFracDigits = 3, minFracDigits = 0) {
+	const locale = "en-US"
+	const notation = (value >= 1e15 || value <= -1e15) ? "scientific" : "compact"
+	return Intl.NumberFormat(locale, {
+		notation: notation,
+		compactDisplay: "short",
+		roundingMode: "trunc",
+		maximumFractionDigits: maxFracDigits,
+		minimumFractionDigits: minFracDigits
+	}).format(value).toLocaleLowerCase();
 }
